84eb76ddda25589c819bf0d79d57ab12953574fb
Min RK <benjaminrk@gmail.com>
2016-08-02 15:17:52 +0200

    refactor backport a bit
    use argparse, gitpython, requests

--- ghtools/backport_pr.py
+++ ghtools/backport_pr.py
@@ -2,24 +2,24 @@
 """
 Backport pull requests to a particular branch.
 
-Usage: backport_pr.py [org/repository] branch [PR] [PR2]
+Usage: backport_pr.py todo [org/repository] branch [PR] [PR2]
 
 e.g.:
 
-    python tools/backport_pr.py 0.13.1 123 155
+    python -m ghtools.backport apply 123 155
 
 to backport PRs #123 and #155 onto branch 0.13.1
 
 or
 
-    python tools/backport_pr.py 2.1
+    python -m ghtools.backport todo --milestone=2.1
 
 to see what PRs are marked for backport with milestone=2.1 that have yet to be applied
 to branch 2.x
 
 or
 
-    python tools/backport_pr.py jupyter/notebook 0.13.1 123 155
+    python -m ghtools.backport --project jupyter/notebook apply 0.13.1 123 155
 
 to backport PRs #123 and #155 of the `jupyter/notebook` repo onto branch 0.13.1
 of that repo.
@@ -28,17 +28,18 @@ of that repo.
 
 from __future__ import print_function
 
+import argparse
+from distutils.version import LooseVersion as V
 import os
 import re
 import sys
 
-from subprocess import Popen, PIPE, check_call, check_output
-try:
-    from urllib.request import urlopen
-except:
-    from urllib import urlopen
+from subprocess import Popen, PIPE
 
-from gh_api import (
+import git
+import requests
+
+from .gh_api import (
     get_issues_list,
     get_pull_request,
     get_pull_request_files,
@@ -46,41 +47,38 @@ from gh_api import (
     get_milestone_id,
 )
 
+
 def find_rejects(root='.'):
     for dirname, dirs, files in os.walk(root):
         for fname in files:
             if fname.endswith('.rej'):
                 yield os.path.join(dirname, fname)
 
-def get_current_branch():
-    branches = check_output(['git', 'branch'])
-    for branch in branches.splitlines():
-        if branch.startswith(b'*'):
-            return branch[1:].strip().decode('utf-8')
 
-def backport_pr(branch, num, project='ipython/ipython'):
-    current_branch = get_current_branch()
+def backport_pr(path, branch, num, project):
+    repo = git.Repo(path)
+    current_branch = repo.active_branch.name
     if branch != current_branch:
-        check_call(['git', 'checkout', branch])
-    check_call(['git', 'pull'])
+        repo.git.checkout(branch)
+    repo.git.pull()
     pr = get_pull_request(project, num, auth=True)
     files = get_pull_request_files(project, num, auth=True)
     patch_url = pr['patch_url']
     title = pr['title']
     description = pr['body']
+    
+    # remove mentions from description, to avoid pings:
+    description = description.replace('@', '_')
+    
     fname = "PR%i.patch" % num
     if os.path.exists(fname):
         print("using patch from {fname}".format(**locals()))
         with open(fname, 'rb') as f:
             patch = f.read()
     else:
-        req = urlopen(patch_url)
-        patch = req.read()
-
-    lines = description.splitlines()
-    if len(lines) > 5:
-        lines = lines[:5] + ['...']
-        description = '\n'.join(lines)
+        r = requests.get(patch_url)
+        r.raise_for_status()
+        patch = r.content
 
     msg = "Backport PR #%i: %s" % (num, title) + '\n\n' + description
     check = Popen(['git', 'apply', '--check', '--verbose'], stdin=PIPE)
@@ -95,14 +93,12 @@ def backport_pr(branch, num, project='ipython/ipython'):
                 f.write(patch)
         return 1
 
-    p = Popen(['git', 'apply'], stdin=PIPE)
-    a,b = p.communicate(patch)
+    p = Popen(['git', 'apply'], cwd=path, stdin=PIPE)
+    p.communicate(patch)
 
     filenames = [ f['filename'] for f in files ]
-
-    check_call(['git', 'add'] + filenames)
-
-    check_call(['git', 'commit', '-m', msg])
+    repo.git.add(*filenames)
+    repo.git.commit('-m', msg)
 
     print("PR #%i applied, with msg:" % num)
     print()
@@ -110,40 +106,31 @@ def backport_pr(branch, num, project='ipython/ipython'):
     print()
 
     if branch != current_branch:
-        check_call(['git', 'checkout', current_branch])
+        repo.git.checkout(current_branch)
 
     return 0
 
-backport_re = re.compile(r"(?:[Bb]ackport|[Mm]erge).*#(\d+)")
+backport_re = re.compile(r"(?:[Bb]ackport|[Mm]erge).*?(\d+)")
 
-def already_backported(branch, since_tag=None):
+
+def already_backported(repo, branch, since_tag=None):
     """return set of PRs that have been backported already"""
     if since_tag is None:
-        since_tag = check_output(['git','describe', branch, '--abbrev=0']).decode('utf8').strip()
-    cmd = ['git', 'log', '%s..%s' % (since_tag, branch), '--oneline']
-    lines = check_output(cmd).decode('utf8')
+        since_tag = repo.git.describe(branch, '--abbrev=0')
+    lines = repo.git.log('%s..%s' % (since_tag, branch), '--oneline')
+    print(lines)
+
     return set(int(num) for num in backport_re.findall(lines))
 
-def should_backport(labels=None, milestone=None, project='ipython/ipython'):
+def should_backport(project, milestone=None):
     """return set of PRs marked for backport"""
-    if labels is None and milestone is None:
-        raise ValueError("Specify one of labels or milestone.")
-    elif labels is not None and milestone is not None:
-        raise ValueError("Specify only one of labels or milestone.")
-    if labels is not None:
-        issues = get_issues_list(project,
-                labels=labels,
-                state='closed',
-                auth=True,
-        )
-    else:
-        milestone_id = get_milestone_id(project, milestone,
-                auth=True)
-        issues = get_issues_list(project,
-                milestone=milestone_id,
-                state='closed',
-                auth=True,
-        )
+    milestone_id = get_milestone_id(project, milestone,
+            auth=True)
+    issues = get_issues_list(project,
+            milestone=milestone_id,
+            state='closed',
+            auth=True,
+    )
 
     should_backport = set()
     for issue in issues:
@@ -154,36 +141,125 @@ def should_backport(labels=None, milestone=None, project='ipython/ipython'):
         if not pr['merged']:
             print ("Marked PR closed without merge: %i" % pr['number'])
             continue
-        if pr['base']['ref'] != 'master':
-            continue
         should_backport.add(pr['number'])
     return should_backport
 
-if __name__ == '__main__':
-    project = 'ipython/ipython'
-    args = list(sys.argv)
-    if len(args) >= 2:
-        if '/' in args[1]:
-            project = args[1]
-            del args[1]
 
-    if len(args) < 2:
-        print(__doc__)
-        sys.exit(1)
+def common_arguments(parser):
+    parser.add_argument('--branch', help="The target branch for backporting")
+    parser.add_argument('--milestone', help="The milestone used to signify what should be backported")
+    parser.add_argument('--project', help="The GitHub project to use (e.g. jupyter/notebook)")
 
-    if len(args) < 3:
-        milestone = args[1]
-        branch = milestone.split('.')[0] + '.x'
-        already = already_backported(branch)
-        should = should_backport(milestone=milestone, project=project)
-        print ("The following PRs should be backported:")
-        for pr in sorted(should.difference(already)):
-            print (pr)
-        sys.exit(0)
+# match jupyter/notebook in:
+# https://github.com/jupyter/notebook.git
+# git@github.com:jupyter/notebook.git
+
+_project_pat = re.compile('.*github.com[:/](.*)\.git', re.IGNORECASE)
+
+def guess_project(path='.'):
+    """Guess the GitHub project
     
-    for prno in map(int, args[2:]):
-        print("Backporting PR #%i" % prno)
-        rc = backport_pr(args[1], prno, project=project)
-        if rc:
-            print("Backporting PR #%i failed" % prno)
-            sys.exit(rc)
+    First, check upstream for people who use:
+    
+    - upstream=project, origin=mine
+    
+    Then, use origin for:
+    
+    - origin=project, mine=mine
+    
+    """
+    repo = git.Repo(path)
+    remotes = [ r for r in repo.remotes if r.name == 'upstream' ]
+    if not remotes:
+        remotes = [ r for r in repo.remotes if r.name == 'origin' ]
+    remote = remotes[0]
+    project = _project_pat.match(remote.url).group(1)
+    return project
+
+
+def tobackport(project, branch, milestone, since):
+    already = already_backported(git.Repo('.'), branch, since)
+    should = should_backport(project, milestone)
+    
+    todo = should.difference(already)
+    shouldnt = already.difference(should)
+    ok = already.intersection(should)
+    print('')
+    print(already,)
+    if shouldnt:
+        still_shouldnt = []
+        for num in sorted(shouldnt):
+            pr = get_pull_request(project, num, auth=True)
+            if pr['milestone'] and V(pr['milestone']['title']) < V(milestone):
+                # ok, marked for backport to an earlier release
+                ok.add(num)
+            else:
+                still_shouldnt.append(num)
+        if still_shouldnt:
+            print("The following PRs have been backported, but are not marked for backport:")
+            for pr in still_shouldnt:
+                print(pr)
+    if ok:
+        print("The following PRs have been backported")
+        for pr in sorted(ok):
+            print(pr)
+    if todo:
+        print ("The following PRs should be backported:")
+        for pr in sorted(todo):
+            print(pr)
+    else:
+        print("Everything appears up-to-date")
+
+def backport():
+    parser = argparse.ArgumentParser(""""
+    Backport a pull request onto a particular branch.
+    
+    Usage:
+    """)
+    parser.add_argument('branch', help="The target branch for backporting. Default: milestone major version.x")
+    parser.add_argument('pulls', nargs='+', type=int, help="The pull requests to backport")
+    parser.add_argument('--project', '-p', help="The GitHub project name. If not specified, guess based on git upstream or origin remotes.")
+    
+
+def main():
+    parser = argparse.ArgumentParser("""
+        Backport pull requests from GitHub projects.
+        
+        Use `todo` to show PRs that need backporting.
+        Use `apply` to apply backports
+    """)
+    parser.add_argument('--project', '-p', help="The GitHub project name. If not specified, guess based on git upstream or origin remotes.")
+    subparsers = parser.add_subparsers(help='subcommand help', dest='action')
+    todo_parser = subparsers.add_parser('todo', help="Show a list of pull requests that need backporting for a particular release.")
+    todo_parser.add_argument('--milestone', '-m', help="The milestone to check for backporting", required=True)
+    todo_parser.add_argument('--branch', '-b', help="The target branch for backporting. Default: milestone major version.x")
+    todo_parser.add_argument('--since', help="The since-tag for checking whether pull requests have been backported `git describe` is used by default.")
+
+    apply_parser = subparsers.add_parser('apply', help="Show a list of pull requests that need backporting for a particular release.")
+    apply_parser.add_argument('branch', help="The target branch for backporting. Default: milestone major version.x")
+    apply_parser.add_argument('pulls', nargs='+', type=int, help="The pull requests to backport")
+    opts = parser.parse_args()
+    if not opts.action:
+        sys.exit("Specify one of `todo` or `action` to perform.")
+    
+    path = '.'
+    milestone = opts.milestone
+    if opts.branch:
+        branch = opts.branch
+    else:
+        branch = milestone.split('.')[0] + '.x'
+    if opts.project:
+        project = opts.project
+    else:
+        project = guess_project(path)
+    
+    if opts.action == 'apply':
+        for pr in opts.pulls:
+            print("Backport PR#{pr} onto {branch}".format(pr=pr, branch=branch))
+            if backport_pr(path, project, branch, pr):
+                sys.exit("Backporting PR#{pr} onto {branch} failed".format(pr=pr, branch=branch))
+    elif opts.action == 'todo':
+        tobackport(project=project, branch=branch, milestone=milestone, since=opts.since)
+
+if __name__ == '__main__':
+    main()
