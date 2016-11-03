ce610c175b35be1b82f67827a1cd79d11145c6ec
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-11-03 15:07:27 -0700

    Api improvement (handle 422, store login)
    As well as a few cleanup if unused import

--- ghpro/api.py
+++ ghpro/api.py
@@ -14,6 +14,8 @@ import requests
 import getpass
 import json
 
+import keyring
+
 try:
     import requests_cache
 except ImportError:
@@ -37,20 +39,46 @@ class Obj(dict):
         self[name] = val
 
 token = None
-def get_auth_token():
-    global token
+user = None
 
-    if token is not None:
-        return token
+def set_username_and_token(*, project, username, token):
+    keyring.set_password('ghlogin', project, user)
+    keyring.set_password('github', project, token)
+
+
+def get_auth_token(*, project=fake_username, store=True):
+    """Get the authentication token for current user.
+
+
+    Get it from the keychain if available, otherwise prompt for username, 
+    password, Two Factor Authentication if necessary. Record a token on the keychain
+    for this particular project. And store all the information in the keychain. 
+    """
+    _, token = get_username_and_auth_token(project=project, store=store)
+    return token
+
+def get_username_and_auth_token(*, project=fake_username, store=True):
+    """Get username and the authentication token for GitHub.
+
+    If stored in the keyring, get them and return it. Other wise prompt for it,
+    as well as 2fa if necessary and store them in keyring before returning it.
+    """
+    global token
+    global user
+
+    if token is not None and user is not None:
+        return user,token
 
     import keyring
-    token = keyring.get_password('github', fake_username)
-    if token is not None:
-        return token
+    token = keyring.get_password('github', project)
+    user = keyring.get_password('ghlogin', project)
+    if token is not None and user is not None:
+        return user, token
 
-    print("Please enter your github username and password. These are not "
+    print("Please enter your github login and password.  Password is not "
           "stored, only used to get an oAuth token. You can revoke this at "
-          "any time on Github.\n"
+          "any time on Github. At the following URL:"
+          "https://github.com/settings/tokens\n"
           "Username: ", file=sys.stderr, end='')
     user = input('')
     pw = getpass.getpass("Password: ", stream=sys.stderr)
@@ -60,8 +88,8 @@ def get_auth_token():
         "public_repo",
         "gist"
       ],
-      "note": "IPython tools",
-      "note_url": "https://github.com/ipython/ipython/tree/master/tools",
+      "note": "Auth requests for %s" % project,
+      "note_url": "https://github.com/minrk/ghpro",
     }
     response = requests.post('https://api.github.com/authorizations',
                             auth=(user, pw), data=json.dumps(auth_request))
@@ -70,13 +98,17 @@ def get_auth_token():
         print("Your login API requested a one time password", file=sys.stderr)
         otp = getpass.getpass("One Time Password: ", stream=sys.stderr)
         response = requests.post('https://api.github.com/authorizations',
-                            auth=(user, pw), 
+                            auth=(user, pw),
                             data=json.dumps(auth_request),
                             headers={'X-GitHub-OTP':otp})
+
+    if response.status_code == 422:
+        print("Such a token is already set on GitHub for %s see https://github.com/settings/tokens"% project)
     response.raise_for_status()
     token = json.loads(response.text)['token']
-    keyring.set_password('github', fake_username, token)
-    return token
+    if store:
+        set_username_and_token(project, user, token)
+    return user, token
 
 def make_auth_header():
     return {'Authorization': 'token ' + get_auth_token()}
